153f3f23f3763c5bd54ffe4dc9be562bfde7317f
Sadia Amena <naazhania917@gmail.com>
2025-11-24 20:55:06 +0530

    initial commit

---
+++ src/main/java/org/example/data_type/PrimitiveDataType.java
@@ -0,0 +1,21 @@
+package org.example.dat_type;
+public class PrimitiveDataType{
+    public static void main(String[] args){
+        int myInt=10;
+        System.out.println("myInt="+myInt);
+        float myFloat=10.5f;
+        System.out.println("myFloat="+myFloat);
+        char myChar='a';
+        System.out.println("myChar="+myChar);
+        boolean myBool=true;
+        System.out.println("myBool="+myBool);
+        double myDouble=20.99;
+        System.out.println("myDouble="+myDouble);
+        long myLong=150000L;
+        System.out.println("myLong="+myLong);
+        short myShort=1000;
+        System.out.println("myShort="+myShort);
+        byte myByte=100;
+        System.out.println("myByte="+myByte);
+    }
+}
