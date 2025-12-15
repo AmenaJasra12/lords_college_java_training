ca26a432a2dd1fb93047b9221bb3f6f89178809e
Sadia Amena <naazhania917@gmail.com>
2025-12-16 00:38:10 +0530

    completed module 2

---
+++ src/main/java/org/example/module_2/assignment/Calculator.java
@@ -0,0 +1,50 @@
+package lords_college_java_training.src.main.java.org.example.module_2.assignment;
+import java.util.Scanner;
+public class Calculator{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean continueCalculation = true;
+
+        while(continueCalculation){
+            System.out.println("Choose your operation: +, -, *, /");
+            String userChoice = sc.next();
+            System.out.println("Enter your first number:");
+            double firstNumber = sc.nextDouble();
+            System.out.println("Enter your second number:");
+            double secondNumber = sc.nextDouble();
+
+            double result = 0;
+            switch (userChoice) {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    if(secondNumber == 0) {
+                        System.out.println("Error: Division by zero is not allowed.");
+                        break;
+                    }
+                    System.out.println("Performing integer division.");
+                    result = firstNumber / secondNumber;
+                    break;
+                default:
+                    System.out.println("Invalid operation selected.");
+            }
+            System.out.println("Result: " + result);
+
+            System.out.println("Do you want to perform another calculation? (yes/no): ");
+            String userResponse = sc.next();
+            if(userResponse.equals("yes")) {
+                continueCalculation = true;
+            } else {
+                continueCalculation = false;
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/module_2/assignment/SimpleStudentApp.java
@@ -0,0 +1,27 @@
+package lords_college_java_training.src.main.java.org.example.module_2.assignment;
+import java.util.Scanner;
+public class SimpleStudentApp {
+    public static void main(String[] args) {
+        Scanner obj =new Scanner(System.in);
+
+        System.out.println("Enter Student Name:");
+        String name=obj.nextLine();
+        System.out.println("Enter Student Gender:");
+        String gender=obj.nextLine();
+        System.out.println("Enter Student Age:");
+        int age=obj.nextInt();
+        System.out.println("Enter your height: ");
+        double height = obj.nextDouble();
+        System.out.println("Enter your weight: ");
+        float weight = obj.nextFloat();
+        System.out.println("Enter your gpa: ");
+        double gpa = obj.nextDouble();
+
+        double bmi = weight / (height * height);
+        System.out.println("Your BMI is " + bmi);
+
+        String result = gpa >= 2.5 ? "Pass" : "Fail";
+        System.out.println("Result " + result);
+
+    }
+}
